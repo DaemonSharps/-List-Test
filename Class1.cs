cbb723cf47785e271d9deed4cac979790c8ba974
Денис Смирнов <66mrpechenka66@gmail.com>
2020-10-10 18:15:34 +0300

    Add a  solution

--- Class1.cs
+++ Class1.cs
@@ -1,11 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace List_Test
 {
     class List
     {
+        private List<int> nums=new List<int>();
+        public List(params int[] par)
+        {
+            if (par.Length != 0)
+            {
+                for (int i = 0; i < par.Length; i++)
+                {
+                    nums.Add(par[i]);
+                }
+            }
+            else
+            {
+                throw new Exception("List must contain at least 1 element.");
+            }
+           
+          
+        }
+        public void ListShow()
+        {
+            foreach (int num in nums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+        public void ListDel(int value)
+        {
+            if (nums.Contains(value))
+            {
+                nums = nums.Where(x => x != value).ToList();
+               
 
+            }
+            
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace List_Test
 {
@@ -7,35 +8,17 @@ namespace List_Test
     {
         static void Main(string[] args)
         {
-            List<int> nums = new List<int>() {1,5,8,3,4,4,234234,153,1234,568,4,681,4,26,2,3,56323,3,64,7989,12,4,7,9 };
-            int value;
-            ListShow(nums);
+            List nums = new List(1,2,2,2,2,2,2,2,2,2,5,5,4,4,22,6,7);
+
+            nums.ListShow();
             Console.Write("Print number you want to delete: ");
-            value=int.Parse(Console.ReadLine());
-            ListDel(nums,value);
-            ListShow(nums);
+           
+            nums.ListDel(int.Parse(Console.ReadLine()));
+            nums.ListShow();
             Console.ReadKey();
 
-            void ListShow(List<int> nums)
-            {
-                foreach (int num in nums)
-                {
-                    Console.WriteLine(num);
-                }
-            }
-            void ListDel(List<int> list, int val)
-            {
-                if (list.Contains(val))
-                {
-                    foreach (int num in list)
-                    {
-                        if (num == val)
-                        {
-                            list.Remove(val);
-                        }
-                    }
-                }
-            }
+            
+           
         }
        
         
